58c4bbf63afb64fec137713f25bc6a69a870288a
igraciarena <ivanigraciarena@gmail.com>
2023-02-27 17:26:54 -0300

    Adding hibernate course initializer

--- src/main/java/igraciarena/hibernatecourse/bootstrap/DataInitializer.java
+++ src/main/java/igraciarena/hibernatecourse/bootstrap/DataInitializer.java
@@ -15,15 +15,10 @@ public class DataInitializer implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         var book = new Book("Domain Drive Design", "123", "RandomHouse");
         bookRepository.save(book);
         var bookSia = new Book("Spring in Action", "234234", "O'relly");
         bookRepository.save(bookSia);
-
-        //        bookRepository.findAll().forEach(book1 -> {
-        //            System.out.println("Book ID:" + book1.getId());
-        //            System.out.println("Book Title:" + book1.getTitle());
-        //        });
     }
 }
