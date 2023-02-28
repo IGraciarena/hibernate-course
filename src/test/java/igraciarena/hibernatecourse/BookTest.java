e1d8fc0f0b8ef4cfbe94790b5eb38ae74b1f59b3
igraciarena <ivanigraciarena@gmail.com>
2023-02-28 12:11:07 -0300

    adding test configurations for Datainit or test execution order

---
+++ src/test/java/igraciarena/hibernatecourse/BookTest.java
@@ -0,0 +1,46 @@
+package igraciarena.hibernatecourse;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import igraciarena.hibernatecourse.domain.Book;
+import igraciarena.hibernatecourse.repository.BookRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DataJpaTest
+class BookTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    // El rollback se usa para que no rollbackee spring cuando se corre este test
+    //    @Rollback(value = false) para versiones de spring viejas
+    @Commit
+    // el order es para saber cual test va a ejecutarse primero
+    @Order(1)
+    @Test
+    @DisplayName("Este test corre primero y hace que en el repositorio en tiempo de ejecucion ya haya 1 registro")
+    void testJpaOk() {
+        var countBefore = bookRepository.count();
+        assertThat(countBefore).isZero();
+
+        bookRepository.save(new Book("My Book of Stubs", "1234", "Self"));
+        var countAfter = bookRepository.count();
+
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    @DisplayName("Este test tendria que falla si no fuera porque usamos el rollback arriba y guardamos " +
+            "en tiempo de ejecucion")
+    void testJpaOkTwo() {
+        var countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(1);
+    }
+}
